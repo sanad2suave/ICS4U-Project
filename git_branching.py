2f9c5403177bc81050b22c4386a90a29ff2856da
Sanad Mustafa <sanadm205@gmail.com>
2023-10-07 00:07:56 -0400

    Completed the solution

--- git_branching.py
+++ git_branching.py
@@ -6,3 +6,8 @@ num = int(input("Enter an integer greater than 0: "))
 for divider in range(1, num+1):
     print(f"divider variable is: {divider}.")
     
+
+    if num % divider == 0:
+        #we have a factor!
+        print(f"{divider} is factor for {num}.")
+        
